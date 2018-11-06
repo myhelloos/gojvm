a34d780e03774bc3307f0fd3eee86c3b266d8bf4
Alfred Yuan <yuanxun.cn@gmail.com>
2018-11-06 21:57:13 +0800

    stack instructions

---
+++ instructions/stack/swap.go
@@ -0,0 +1,27 @@
+package stack
+
+import (
+  "jvm-go/instructions/base"
+  "jvm-go/rtda"
+)
+
+// Swap the top two operand stack values
+type SWAP struct {
+  base.NoOperandsInstruction
+}
+
+/*
+bottom -> top
+[...][c][b][a]
+          \/
+          /\
+         V  V
+[...][c][a][b]
+*/
+func (self *SWAP) Execute(frame *rtda.Frame) {
+  stack := frame.OperandStack()
+  slot1 := stack.PopSlot()
+  slot2 := stack.PopSlot()
+  stack.PushSlot(slot1)
+  stack.PushSlot(slot2)
+}
