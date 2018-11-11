bb4375da1d61f26cd6a86cc0a84ee6854fffff38
Alfred Yuan <yuanxun.cn@gmail.com>
2018-11-11 18:17:32 +0800

    read and parse class

--- rtda/heap/class_loader.go
+++ rtda/heap/class_loader.go
@@ -3,6 +3,7 @@ package heap
 import (
   "jvm-go/classpath"
   "fmt"
+  "jvm-go/classfile"
 )
 
 type ClassLoader struct {
@@ -30,4 +31,57 @@ func (self *ClassLoader) loadNonArrayClass(name string) *Class {
  link(class)
  fmt.Printf("[Loaded %s from %s]\n", name, entry)
  return class
+}
+
+func (self *ClassLoader) readClass(name string) ([]byte, classpath.Entry) {
+  data, entry, err := self.cp.ReadClass(name)
+  if err != nil {
+    panic("java.lang.ClassNotFoundException: " + name)
+  }
+  return data, entry
+}
+func (self *ClassLoader) defineClass(data []byte) *Class {
+  class := parseClass(data)
+  class.loader = self
+  resolveSuperClass(class)
+  resolveInterface(class)
+  self.classMap[class.name] = class
+  return class
+}
+
+func parseClass(data []byte) *Class {
+  cf, err := classfile.Parse(data)
+  if err != nil {
+    panic("java.lang.ClassFormatError")
+  }
+  return newClass(cf)
+}
+
+func resolveSuperClass(class *Class) {
+  if class.name != "java/lang/Object" {
+    class.superClass = class.loader.LoadClass(class.superClassName)
+  }
+}
+
+func resolveInterface(class *Class) {
+  interfaceCount := len(class.interfaceNames)
+  if interfaceCount > 0 {
+    class.interfaces = make([]*Class, interfaceCount)
+    for i, interfaceName := range class.interfaceNames {
+      class.interfaces[i] = class.loader.LoadClass(interfaceName)
+    }
+  }
+}
+
+func link(class *Class) {
+  verify(class)
+  prepare(class)
+}
+
+func verify(class *Class) {
+  // todo
+}
+
+func prepare(class *Class) {
+
 }
